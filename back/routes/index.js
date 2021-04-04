545302e956ab81784641b1ff6d162f2f065003a4
mikhail1770 <dvtd.misha@gmail.com>
2021-04-04 11:30:10 +0300

    Установка Express  и настройка подключения к БД

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'univdoc'
+});
+
+connection.connect();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/students/:id', function(req, res, next){
+  connection.query('SELECT * FROM students WHERE id = ?', [req.params.id], function (error, results, fields) {
+    if (error) throw error;
+    res.json(results);
+  });  
+})
 
 module.exports = router;
