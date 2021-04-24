470a6c522fb7f83419c3fc59b9e4360b3390c83e
mikhail1770 <dvtd.misha@gmail.com>
2021-04-24 09:45:35 +0300

    Подключение к БД на хостинге

--- back/routes/index.js
+++ back/routes/index.js
@@ -3,20 +3,42 @@ var router = express.Router();
 var mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'univdoc'
+  host: 'server9.hosting.reg.ru',
+  user: 'u0856139_univdoc',
+  password: 'UnivDoc71',
+  database: 'u0856139_univdoc'
 });
 
 connection.connect();
 
 /* GET home page. */
-router.get('/students/:id', function(req, res, next){
+router.get('/students/:id', function(req, res, next){ //запрос данных студента по id
   connection.query('SELECT * FROM students WHERE id = ?', [req.params.id], function (error, results, fields) {
     if (error) throw error;
     res.json(results);
   });  
 })
+
+router.get('/univGroups/:id', function(req, res, next){ //запрос на получение списка группы по id
+  connection.query('SELECT * FROM students WHERE UnivGroup = ?', [req.params.id], function (error, results, fields) {
+    if (error) throw error;
+    res.json(results);
+  });  
+})
+
+router.get('/univGroups', function(req, res, next){ //запрос на получение списка групп
+  connection.query('SELECT * FROM univgroups', function (error, results, fields) {
+    if (error) throw error;
+    res.json(results);
+  });  
+})
+
+router.get('/professor', function(req, res, next){ //запрос на получение списка групп
+  connection.query('SELECT * FROM professor', function (error, results, fields) {
+    if (error) throw error;
+    res.json(results);
+  });  
+})
+
 
 module.exports = router;
